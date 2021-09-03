1c583576e0764c09181a048d8347b92e4480a5c3
Igor <igogithub@gmail.com>
2021-09-03 08:29:34 -0300

    :wrench: Working in only followers exception

--- main.js
+++ main.js
@@ -108,20 +108,31 @@ app.listen(PORT, () => {
               const response = await bitly.shorten(video);
               var link = response.link;
   
-              var res = {
-                status: `Baixei, saporra, @${tweet.user.screen_name}, segue pra dar aquela moral, vai` + `\n${link}`,
-                in_reply_to_status_id: '' + tweet_id
-              };
-            
-              downloader.post('statuses/update', res,
-                function(err, data, response) {
-                  console.log(data);
-                }
-              );
-            }
+              downloader.get('followers/ids', { screen_name: "baixesaporra" },  function (err, data, response) {
+
+                console.log(data)
   
+                data.ids.forEach(id => {
+                  if(tweet.user.id == id) {
+                    reply(protocols[0]);
+                  }
+                  else {
+                    var res = {
+                      status: `Ei, @${tweet.user.screen_name}, meu patrão, agora você precisa me seguir pra eu poder te ajudar 😬`,
+                      in_reply_to_status_id: '' + tweet_id
+                    };
+          
+                    downloader.post('statuses/update', res,
+                    function(err, data, response) {
+                      console.log(data);
+                    }
+                  );
+                  }
+                })
+            })
             reply(protocols[0]);
           }
+        }
       }
     });
   };
